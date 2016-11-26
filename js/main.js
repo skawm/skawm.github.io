bf6bf494b33947be4ed36a73dcaba2bc2ad48164
Max <max.wassler@gmail.com>
2016-11-26 21:41:16 +0100

    formatted code

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
-$(document).ready(function(){
-	$.fn.extend({
-		animateCss: function (animation){
-			var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        	$(this).addClass('animated ' + animation)
-        	$(this).on(animationEnd, function() {
-            	$(this).removeClass('animated ' + animation);
-        	});
-		}
-	});
+$(document).ready(function() {
+    $.fn.extend({
+        animateCss: function(animation) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            $(this).addClass('animated ' + animation)
+            $(this).on(animationEnd, function() {
+                $(this).removeClass('animated ' + animation);
+            });
+        }
+    });
 
 
-    
-	var time = new Date();
-	//$('#hi').addClass('hidden').delay(1000).removeClass('hidden').animateCss('bounceIn');
-	//$('#hi').hide(0).delay(1000).show(0).animateCss('bounceIn');
-	setTimeout(function () {
-		$('#hi').removeClass('hidden');
-		$('#hi').animateCss('tada');
-	}, 1000)
-    setTimeout(function () {
-		$('.intro').removeClass('hidden');
-		$('.intro').animateCss('fadeIn');
-	}, 2300)
-    setTimeout(function () {
-		$('.fa-twitter').removeClass('hidden');
-		$('.fa-twitter').animateCss('bounceIn');
-	}, 3000)
-    setTimeout(function () {
-		$('.fa-github').removeClass('hidden');
-		$('.fa-github').animateCss('bounceIn');
-	}, 3200)
+
+    var time = new Date();
+    //$('#hi').addClass('hidden').delay(1000).removeClass('hidden').animateCss('bounceIn');
+    //$('#hi').hide(0).delay(1000).show(0).animateCss('bounceIn');
+    setTimeout(function() {
+        $('#hi').removeClass('hidden');
+        $('#hi').animateCss('tada');
+    }, 1000)
+    setTimeout(function() {
+        $('.intro').removeClass('hidden');
+        $('.intro').animateCss('fadeIn');
+    }, 2300)
+    setTimeout(function() {
+        $('.fa-twitter').removeClass('hidden');
+        $('.fa-twitter').animateCss('bounceIn');
+    }, 3000)
+    setTimeout(function() {
+        $('.fa-github').removeClass('hidden');
+        $('.fa-github').animateCss('bounceIn');
+    }, 3200)
 
 });
